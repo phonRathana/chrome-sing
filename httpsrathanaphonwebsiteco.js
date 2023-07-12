35f6a05bc866d13236f12f56cb9433ff7f522fd6
Phon Rathana <127619322+phonRathana@users.noreply.github.com>
2023-07-13 02:13:42 +0700

    Update httpsrathanaphonwebsiteco.js

--- httpsrathanaphonwebsiteco.js
+++ httpsrathanaphonwebsiteco.js
@@ -1,23 +1,4 @@
 
-
-to property="og:type" content="website"meta name="theme-color" wm #039be5meta charset="utf-8">000
- ceta content="IE-Edge" http-equiv="X-UA-Compatible"> seta nanes"vimport" content="width=device-width, initial-scales       
-   <meta name="document-type" content="Public">
-   <meta name="document-rating" content="Safe for Kids">
-   <meta name="robots" content="ALL, INDEX, FOLLOW">
-   <meta name="googlebot" content="index, follow">
-   <meta name="mobile-web-app-capable" content="yes">
-   <meta name="apple-mobile-web-app-capable" content="yes">
-   <link rel="apple-touch-icon" href="https://httpsrathanaphon.websitesco./files/971938/favicon/favicon.png?v=528721588
-   <meta name="theme-color" content="#FFFFFF">
-   <meta name="viewport" content="width=device-width, initial-scale=1, maximum
-   <meta name="title" content="Rathana phone Shop1 in  Phnom Penh">
-   <meta name="description" content="Business As a loyal partner sell technology">
-   <meta name="author" content="Rathana phone Shop1">
-   <meta name="keywords" content="Rathana phone Shop1,Rathana phone Shop1 in Phnom Penh,Rathana phone Shop1 in Phnom Penh,Rathana phone Shop1 in Cambodia,Rathana phone Shop1 in OTHER RETAIL">
-   <meta name="viewport" content="width=device-
-	address": {
-            "@type": "PostalAddress",
              
 
 	
