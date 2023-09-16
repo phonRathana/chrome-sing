e84c62faf273d42e2c09ca448227f90da5e14139
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-16 19:33:44 +0700

    Update Preload.js

--- Preload.js
+++ Preload.js
@@ -1,10 +1,241 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
+<!DOCTYPE html>  
+  
+<html>  
+    <head>  
+   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+   <meta name="description" content="Rathana.Phone.Shop1">  
+   <meta name="keywords" content="Business As a loyal partner">  
+   <meta name="author" content="https://httpsrathanaphon.website.co.in"/>  
+   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >    
+   <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">    
+   <title>Rathana.Phone.shop1</title>  
+   <link rel="stylesheet" href="./css/style.css">  
+   <style type="text/css">   
+     body{
+       font: 15px/1.5 Arial, Helvetica,sans-serif;
+       padding:0;
+       margin:0;
   }
+   .container {
+      width:80%;
+      margin:auto;
+      overflow:hidden;
+  }
+   ul {
+      margin:0;
+      padding:0;
+  }
+  .button_1 {
+     height:38px;
+     background:#e8491d;
+     border:0;
+     padding-left: 20px;
+     padding-right:20px;
+     color:#ffffff;
+  }
+  .dark {
+     padding:15px;
+     background:#35424a;
+     color:#ffffff;
+     margin-top:10px;
+     margin-bottom:10px;
+  }
+   header {
+     background:#35424a;
+     color:#ffffff;
+     padding-top:30px;
+     min-height:70px;
+     border-bottom:#e8491d 3px solid;
+ }
+   header a {
+    color:#ffffff;
+    text-decoration:none;
+    text-transform: uppercase;
+    font-size:16px;
+ }
+   header li {
+    float:left;
+    display:inline;
+    padding: 0 20px 0 20px;
+ }
+  header #branding {
+    float:left;
+ }
+  header #branding h1 {
+    margin:0;6g
+ }
+  header nav {
+    float:right;
+    margin-top:10px;
+ }
+  header .highlight, header .current a {
+    color:#e8491d;
+    font-weight:bold;
+ }
+  header a:hover {
+    color:#fffff;
+    font-weight:bold;
+  }
+  imag  h1 {
+    margin-top:100px;
+    font-size:55px;
+    margin-bottom:10px;
+ }
+  newsletter {
+    padding:15px;
+    color:#ffffff;
+    background:#35424a
+  }
+  newsletter h1 {
+    float:left;
+  }
+  newsletter form {
+    float:right;
+    margin-top:15px;
+ }
+  newsletter input[type="email"]{
+    padding:4px;
+    height:25px;
+    width:250px;
+  }
+  boxes {
+    margin-top:20px;
+  }
+  boxes .box {
+    float:left;
+    text-align: center;
+    width:30%;
+    padding:10px;
+ }
+  boxes .box img {
+   width:90px;
+ }
+  aside#sidebar {
+    float:right;
+    width:30%;
+    margin-top:10px;
+ }
+  aside#sidebar .quote input, aside#sidebar .quote textarea{
+    width:90%;
+    padding:5px;
+ }
+  article#main-col {
+    float:left;
+    width:65%;
+ }
+  ul#services li {
+    list-style: none;
+    padding:20px;
+    border: #cccccc solid 1px;
+    margin-bottom:5px;
+    background:#e6e6e6;
+ }
+  footer {
+    padding:20px;
+    margin-top:20px;
+    color:#ffffff;
+    background-color:#e8491d;
+    text-align: center;
+  }
+   header {
+    padding-bottom:20px;
+  }
+   showcase h1 {
+     margin-top:40px;
+  }
+   newsletter button, .quote button {
+     display:block;
+     width:100%;
+  }
+   newsletter form input[type="email"], .quote input, .quote textarea{
+     width:100%;
+     margin-bottom:5px;
+  }
+    </style>
+ </head>
+ <body>
+    <header>
+        <div id="branding"/>
+          <h1><span class="light"></span></h1>
+        </div>
+    </div>
+        </nav>
+     </div>
+     <div class="container-fluid">
+          <div class="row">
+            </ul>
+         </div>
+    </heard>
+       <footer>
+    <section id="container">
+        <h1>® Rathana.phone.Shop1</h1>
+        </footer>
+      </div>
+    </section>
+    <section id="main">
+       <div class="container">
+        <article id="main-col">
+         <h1 class="page-title">hello would</h1>
+       <ul id="services">
+        <li>
+           <h3> Web design </h3>
+        <p></p>
+          <br>
+             <img src="https://picsum.photos/400/300" width="100%">
+          <br>
+       </ul>
+     </article>
+        <aside id="sidebar">
+            <div class="dark">
+         <h3> contact </h3>
+       <form class="quote">
+  			 <div>
+  		   	<label>Name</label><br>
+  			  <input type="text" placeholder="Name">
+  		</div>
+  			<div>
+  		 	 <label>Last Name</label><br>
+  			 <input type="text" placeholder="Last Name">
+  		</div>
+  			 <div>
+  		   	<label>Email</label><br>
+  				<input type="email" placeholder="Email Address">
+  		</div>
+  		 	<div>
+  				<label>Message</label><br>
+  				<textarea placeholder="Message"></textarea>
+  		</div>
+  			<div>
+  			 	<button class="button_2" type="submit">Send</button>
+  			 </div>
+	  </form>
+  </section>
+ <footer><footer>
+ <header></header>
+   <main></main>
+      <div>
+         </div>
+      <div class="footer-body">
+         <div>® Rathana.phone.Shop1</div>
+         <div>
+          <p>យើងផ្តល់ជូននូវជម្រើសដ៏ធំទូលាយនៃស្មាតហ្វូន និងគ្រឿងបន្លាស់ចុងក្រោយបំផុតពីម៉ាកសំខាន់ៗទាំងអស់។  បុគ្គលិកដែលមានចំណេះដឹងរបស់យើងអាចជួយអ្នកក្នុងការស្វែងរកឧបករyណ៍ដ៏ល្អឥតខ្ចោះដែលត្រូវនឹងតម្រូវការ និងថវិការបស់អ្នក</p> 
+              <ul class="list-group">
+                <li><a href="#"><i class="fa fa-facebook"></i> Facebook </li>
+                <li><a href="#"><i class="fa fa-twitter"></i> Twitter </li>
+                <li><a href="#"><i class="fa fa-google-plus"></i> Google Plus </li>
+                <li><a href="#"><i class="fa fa-linkedin"></i> LinkedIn </li>
+            </ul>
+         </div>
+      </div>
+    </div>
+      <div class="footer-copyright"/>
+    </div>
+  </footer>
+</body>
+</html>
+   
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+
+     
+  
